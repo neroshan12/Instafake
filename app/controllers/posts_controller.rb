4549c882d5c98f53b0dfdf38b924245d2fd20aad
Nero Siva <neroshan12@gmail.com>
2017-10-22 20:26:10 +0100

    Add image uploads

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,27 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def display
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    @post.save
+    redirect_to @post
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :description, :image)
+  end
+
 end
